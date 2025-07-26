56f6d113bf17256774e91f679dbc4a8e5a4f8907
tw30006 <original.heart.2016@gmail.com>
2025-07-26 23:33:38 +0800

    feat: readme&base url

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  base: '/everiyn-ec/',
   server: {
     proxy: {
       '/v2/api': {
